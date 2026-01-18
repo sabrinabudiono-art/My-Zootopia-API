992eefa004fd02ee1f376a27350d12b0b224ecda
Sabrina <sabrinabudiono@gmail.com>
2026-01-18 03:43:56 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,15 +5,38 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-animals_data = load_data("animals_data.json")
-
 def print_animals():
+    animals_data = load_data("animals_data.json")
+    output = ""
     for animal in animals_data:
-        print(f"Name: {animal["name"]}")
-        print(f"Diet: {animal["characteristics"]["diet"]}")
-        print(f"Location: {animal["locations"][0]}")
+        output += f"Name: {animal["name"]}\n"
+        output += f"Diet: {animal["characteristics"]["diet"]}\n"
+        output += f"Location: {animal["locations"][0]}\n"
         if "type" in animal.get("characteristics", {}):
-            print(f"Type: {animal["characteristics"]["type"]}")
-        print()
+            output += f"Type: {animal["characteristics"]["type"]}\n"
+        output += "\n"
+    return output
 
-print_animals()
+
+def load_html():
+  """ Loads a html file """
+  with open("animals_template.html", "r") as fileobj:
+      return fileobj.read()
+
+
+def replace_info(html):
+    animals_info = print_animals()
+    return html.replace("__REPLACE_ANIMALS_INFO__", animals_info)
+
+def write_html(data):
+    with open("animals.html", "w") as fileobj:
+        fileobj.write(data)
+
+def main():
+    html = load_html()
+    new_html = replace_info(html)
+    write_html(new_html)
+
+
+if __name__ == "__main__":
+    main()
